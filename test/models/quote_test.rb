680fe1893ccf75c5f3ca4b03db4e124d34b5cd0a
Brandon <handlez36@gmail.com>
2015-10-29 11:30:39 +0000

    Added author initial in the index page and test to ensure the model correctly creates the initials

--- test/models/quote_test.rb
+++ test/models/quote_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class QuoteTest < ActiveSupport::TestCase
-   test "the truth" do
-     assert true
-   end
+  
+  test "unique_tag" do
+    quote = Quote.create(:author => 'Brandon McLean', :saying => 'Time to get it in')
+    expected = 'BM#' + quote.id.to_s
+    actual = quote.unique_tag
+    assert_equal expected, actual
+  end
+  
+  
 end
