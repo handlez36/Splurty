2c8b89636a448f9d7672f642932976e576a049b5
Brandon <handlez36@gmail.com>
2015-11-01 22:32:17 +0000

    Added FactoryGirl gem and disabled use of fixtures

--- test/controllers/quotes_controller_test.rb
+++ test/controllers/quotes_controller_test.rb
@@ -2,7 +2,8 @@ require 'test_helper'
 
 class QuotesControllerTest < ActionController::TestCase
   test "quote show page" do
-    quote = Quote.create(:author=>'Brandon McLean', :saying => 'Like a boss')
+    #quote = Quote.create(:author=>'Brandon McLean', :saying => 'Like a boss')
+    quote = FactoryGirl.create(:quote)
     get :show, :id => quote.id
     assert_response :success
    end
--- test/models/quote_test.rb
+++ test/models/quote_test.rb
@@ -3,7 +3,8 @@ require 'test_helper'
 class QuoteTest < ActiveSupport::TestCase
   
   test "unique_tag" do
-    quote = Quote.create(:author => 'Brandon McLean', :saying => 'Time to get it in')
+    #quote = Quote.create(:author => 'Brandon McLean', :saying => 'Time to get it in')
+    quote = FactoryGirl.create(:quote, :author => "Brandon McLean")
     expected = 'BM#' + quote.id.to_s
     actual = quote.unique_tag
     assert_equal expected, actual
